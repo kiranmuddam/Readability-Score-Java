ee3479fdc2de4cda2af45c5f851b3b9d2e1b6fec
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-11 11:56:12 +0530

    Complete stage 2 "Readability Score".

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -3,9 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int specialCharCount = 0;
-        String textInput = scanner.nextLine();
-        if (textInput.length() > 100) {
+        int totalWordsCount = 0;
+        int totalSentenceCount = 0;
+        String inputText = scanner.nextLine();
+        int strLength = inputText.length();
+        if (inputText.charAt(strLength - 1) != '?' && inputText.charAt(strLength - 1) != '!' && inputText.charAt(inputText.length() - 1) != '.' ) {
+            totalSentenceCount = 1;
+        }
+        String[] textArray = inputText.split("[\\s\\u00a0]");
+        //System.out.println(textArray[0]);
+        //System.out.println(textArray.length);
+        for (int i = 0; i < textArray.length; ++i) {
+            ++totalWordsCount;
+            if (textArray[i].matches(".+[!?.]")) {
+                ++totalSentenceCount;
+            }
+        }
+        //System.out.println(totalWordsCount);
+        //System.out.println(totalSentenceCount);
+        double myCount = (double) totalWordsCount / totalSentenceCount;
+        if (myCount > 10) {
             System.out.println("HARD");
         } else {
             System.out.println("EASY");
--- Readability Score/task/test/ReadabilityTest.java
+++ Readability Score/task/test/ReadabilityTest.java
@@ -1,53 +1,45 @@
 import org.hyperskill.hstest.stage.StageTest;
-import org.hyperskill.hstest.testcase.CheckResult;
-import org.hyperskill.hstest.testcase.TestCase;
+import org.hyperskill.hstest.testcase.SimpleTestCase;
 import readability.Main;
 
 import java.util.List;
 
 
-public class ReadabilityTest extends StageTest<String> {
+public class ReadabilityTest extends StageTest {
 
     public ReadabilityTest() {
         super(Main.class);
     }
 
     @Override
-    public List<TestCase<String>> generate() {
+    public List<SimpleTestCase> generate() {
+        return List.of(
+            new SimpleTestCase("This text is simple to read! " +
+                "It has on average less than 10 words per sentence.",
+                "EASY"),
 
-        List<TestCase<String>> tests = List.of(
-            new TestCase<String>()
-                .setInput("This text is simple to read!"),
+            new SimpleTestCase("This text is hard to read. " +
+                "It contains a lot of sentences as well as a lot of words in each sentence",
+                "HARD"),
 
-            new TestCase<String>()
-                .setInput("This text is hard to read. " +
-                    "It contains a lot of sentences as well as a lot of words in each sentence."),
+            new SimpleTestCase("1 ".repeat(99) + "1. 1. 1.",
+                "HARD"),
 
-            new TestCase<String>()
-                .setInput("1".repeat(99)),
+            new SimpleTestCase("12, 12 13 14 14 14, " +
+                "12 21 23 89! 75 12, 134 241 123, 123 123 123, 123 123 " +
+                "123? 123, 123 123 123 23 123 213 123 123 123.",
+                "HARD"),
 
-            new TestCase<String>()
-                .setInput(" ".repeat(100)),
+            new SimpleTestCase("12, 12 13 14 14 14, 12 21 23 89! " +
+                "75 12, 241 123, 123 123 123, 123 123 123? 123, " +
+                "123 123 123 23 123 213 123 123 123.",
+                "EASY"),
 
-            new TestCase<String>()
-                .setInput("q".repeat(101))
+            new SimpleTestCase( "Readability is the ease with which a " +
+                "reader can understand a written text. In natural language, the " +
+                "readability of text depends on its content and its presentation. " +
+                "Researchers have used various factors to measure readability. And that is it!",
+                "EASY")
         );
-
-        for (TestCase<String> test : tests) {
-            test.setAttach(test.getInput());
-        }
-
-        return tests;
-    }
-
-    @Override
-    public CheckResult check(String reply, String clue) {
-        String solution = solve(clue);
-        boolean isSuccess = reply.strip().equals(solution.strip());
-        return new CheckResult(isSuccess);
-    }
-
-    private String solve(String input) {
-        return input.length() > 100 ? "HARD" : "EASY";
     }
 }
