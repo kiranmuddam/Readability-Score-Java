35208e34ac147ab138c98694480cac7177f4d7b5
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-12 09:04:38 +0530

    Solve problem "Binary Search"

--- Problems/Application as a class/src/Main.java
+++ Problems/Application as a class/src/Main.java
@@ -4,8 +4,8 @@ class Application {
 
     void run(String[] args) {
         System.out.println(name);
-        for (int i = 0; i < args.length; ++i) {
-            System.out.println(args[i]);
+        for (String i : args) {
+            System.out.println(i);
         }
     }
 }
