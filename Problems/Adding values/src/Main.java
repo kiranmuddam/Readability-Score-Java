b8223b2f540db4af5613abdd7286ff9a8a7591b0
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-06-12 08:04:11 +0530

    Solve problem "Adding Values"

---
+++ Problems/Adding values/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+
+    public static void addValueByIndex(long[] array, int index, long value) {
+        array[index] = array[index] + value;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long[] array = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToLong(Long::parseLong)
+                .toArray();
+        int index = scanner.nextInt();
+        long value = scanner.nextLong();
+        addValueByIndex(array, index, value);
+        Arrays.stream(array).forEach(e -> System.out.print(e + " "));
+    }
+}
